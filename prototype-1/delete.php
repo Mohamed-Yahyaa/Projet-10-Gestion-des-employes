6727979d76df03094d43c5ac79c33fc6de9d524a
mohamedyahya <mohamed.yahya.dev@gmail.com>
2022-02-18 11:56:01 +0100

    prototype-1-add

--- prototype-1/delete.php
+++ prototype-1/delete.php
@@ -1,7 +1,17 @@
 <?php 
-  
+      if(isset($_GET['id'])){
+          $id =$_GET['id'];
+          $data = json_decode(file_get_contents('people.json'));
+          for($i= 0; $i < count($data); ++$i){
+              if($data[$i][0]== $id){
+                unset($data[$i]);
 
+                $data = array_values($data);
+                file_put_contents("people.json", json_encode($data));
+                break;
+              }
+            }
 
-
-
+            header('location: index.php');
+      }
 ?>
