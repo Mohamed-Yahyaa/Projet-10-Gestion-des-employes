8a7bb8f9603e7cf72112fd25a00473e76622bd26
mohamedyahya <mohamed.yahya.dev@gmail.com>
2022-02-25 10:13:47 +0100

    update

--- prototype 3/employee.php
+++ prototype 3/employee.php
@@ -1,14 +1,8 @@
 <?php
     class Employee {
         private $id;
-<<<<<<< HEAD
         private $fname;
         private $lname;
-=======
-        private $firstName;
-        private $lastName;
-        private $gender;
->>>>>>> 3bf99ed5edb18d1b1107d6b39589fb98974a404b
         private $age;
 
         public function getId(){
@@ -18,7 +12,6 @@
             $this->id = $value;
         }
 
-<<<<<<< HEAD
         public function getfname(){
             return $this->firstName;
         }
@@ -42,37 +35,6 @@
         }
 
         public function setage($value){
-=======
-        public function getFirstName(){
-            return $this->firstName;
-        }
-
-        public function setFirstName($value){
-            $this->firstName = $value;
-        }
-
-        public function getLastName(){
-            return $this->lastName;
-        }
-
-        public function setLastName($value){
-            $this->lastName= $value;
-        }
-
-        public function getGender(){
-            return $this->gender;
-        }
-
-        public function setGender($value){
-            $this->gender= $value;
-        }
-
-        public function getAge(){
-            return $this->age;
-        }
-
-        public function setAge($value){
->>>>>>> 3bf99ed5edb18d1b1107d6b39589fb98974a404b
             $this->age = $value;
         }
     }
